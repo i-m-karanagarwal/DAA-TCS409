0c6643261451b39111ae7184cfb159f5c72328e1
i-m-karanagarwal <77572300+i-m-karanagarwal@users.noreply.github.com>
2022-02-23 10:10:33 +0530

    Week 1 Question 1

---
+++ Week1/WEEK1QUESTION1.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#define max 20
+
+int main()
+{
+    int T;
+    int arr[max];
+    printf("Enter number of test cases: ");
+    scanf("%d",&T);
+    while(T)
+    {
+        int n,key,i,com=0,flag=0;
+        arr[n];
+        printf("Enter number of elements: ");
+        scanf("%d",&n);
+        printf("Enter elements of the array: ");
+        for(i=0;i<n;i++)
+        {
+            scanf("%d",&arr[i]);
+        }
+        printf("Enter key to be found: ");
+        scanf("%d",&key);
+
+        for(i=0;i<n;i++)
+        {
+            com++;
+            if(key==arr[i])
+            {
+                flag=1;
+                break;
+            }
+        }
+
+        if(flag==1)
+            printf("%d found in %d comparisions\n",key,com);
+        else
+            printf("Key Not found\n");
+        T--;
+    }
+}
