d6d6f9fff9aec420af82667c082bddd87cb02750
i-m-karanagarwal <77572300+i-m-karanagarwal@users.noreply.github.com>
2022-03-23 10:39:40 +0530

    Question 3 added

---
+++ Lab Tutorials/Week2/WEEK2QUESTION3.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+
+    while(t){
+        int n;
+        scanf("%d", &n);
+
+        int arr[n];
+        for(int i=0;i<n;i++)
+            scanf("%d", &arr[i]);
+
+        int k,count=0;
+        scanf("%d", &k);
+
+        for(int i=0;i<n;i++){
+            for(int j=1; j<n; j++)
+                if(arr[j]-arr[i]==k)
+                    {
+                        count++;
+                    }
+        }
+        printf("%d pairs found", count);
+
+        t--;
+    }
+}
